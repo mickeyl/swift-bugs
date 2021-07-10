c1aa8770923e8d16b5f45bc54ec98b374b11b6b1
Dr. Michael Lauer <mickey@vanille-media.de>
2021-07-10 11:15:09 +0200

    upload example for https://bugs.swift.org/browse/SR-14841

---
+++ SR14841/SR14841/CommandQueue.swift
@@ -0,0 +1,68 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+import Foundation
+
+typealias Continuation = CheckedContinuation<String, Error>
+
+public enum StreamError: Error {
+    case invalidEncoding
+}
+
+public class StreamCommand {
+
+    let continuation: Continuation
+
+    init(continuation: Continuation) {
+        self.continuation = continuation
+    }
+
+    func resumeContinuation() {
+        let response = "fooBar"
+        print("<triggering continuation resume> \(self.continuation)")
+        self.continuation.resume(returning: response)
+        print("</triggering continuation resume>")
+    }
+}
+
+actor StreamCommandQueue {
+
+    var activeCommand: StreamCommand?
+
+    func send(string: String, timeout: TimeInterval) async throws -> String {
+
+        print("awaiting...")
+        let response: String = try await withCheckedThrowingContinuation { continuation in
+            print("continuation: \(continuation)")
+            self.activeCommand = StreamCommand(continuation: continuation)
+            self.outputActiveCommand()
+        }
+        print("came back after awaiting")
+        return response
+    }
+
+    func outputActiveCommand() {
+        async {
+            self.activeCommand?.resumeContinuation()
+        }
+    }
+
+    func inputActiveCommand() {
+
+    }
+}
+
+func doIt() {
+
+    async {
+        let streamQueue = StreamCommandQueue()
+        do {
+            let identification = try await streamQueue.send(string: "ATI\r", timeout: 1)
+            print("identification: \(identification)")
+        } catch {
+            print("can't get identification: \(error)")
+        }
+    }
+
+}
+
---
+++ SR14841/SR14841/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SR14841
+//
+//  Created by Dr. Michael Lauer on 10.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        doIt()
+    }
+
+
+}
+
